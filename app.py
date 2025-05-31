72e93a486f360a6e91694359b1746b23430283b8
wvdh <wanda@vandenhoogen.eu>
2025-05-31 13:54:47 +0200

    changed app.py

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
-from flask import Flask, request, jsonify
+
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 def get_bmi_interpretation(bmi):
-    if bmi <= 18.4:
-        return 'You are underweight.'
-    elif bmi <= 24.9:
-        return 'Your weight is normal.'
-    elif bmi <= 29.9:
-        return 'You are slightly overweight.'
-    elif bmi <= 34.9:
-        return 'You are severely overweight.'
-    elif bmi <= 39.9:
-        return 'You are obese.'
-    else:
-        return 'You are severely obese.'
+	if bmi <= 18.4:
+		return 'You are underweight.'
+	elif bmi <= 24.9:
+		return 'Your weight is normal.'
+	elif bmi <= 29.9:
+		return 'You are slightly overweight.'
+	elif bmi <= 34.9:
+		return 'You are severely overweight.'
+	elif bmi <= 39.9:
+		return 'You are obese.'
+	else:
+		return 'You are severely obese.'
 
-@app.route('/calculate_bmi', methods=['POST'])
-def calculate_bmi():
-    data = request.json
-    weight = float(data['weight'])
-    height = float(data['height'])
-    bmi = round(weight / (height ** 2))
-    interpretation = get_bmi_interpretation(bmi)
-
-    return jsonify({"bmi": bmi, "interpretation": interpretation})
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+		weight = float(request.form['weight'])
+		height = float(request.form['height'])
+		bmi = round(weight / (height ** 2), 2)
+		interpretation = get_bmi_interpretation(bmi)
+		return render_template('result.html', bmi=bmi, interpretation=interpretation)
+	return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
